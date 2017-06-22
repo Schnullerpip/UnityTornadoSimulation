8ad82cf14dec1e5877f3e243daa758bc5eba275f
Julian <Schnullerpip@gmail.com>
2017-06-22 10:24:53 +0200

    first stupid implementation

---
+++ Assets/Scripts/Caught.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caught : MonoBehaviour
+{
+    private TornadoScript tornadoReference;
+    private SpringJoint spring;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+        transform.RotateAround(tornadoReference.transform.position, tornadoReference.GetRotationAxis(), tornadoReference.GetTornadoStrength());
+
+
+    }
+
+    //call this when tornadoReference already exists
+    public void Init(TornadoScript tornadoRef, float springForce, float damper, float maxDistance, float minDistance)
+    {
+        //save tornado reference
+        tornadoReference = tornadoRef;
+
+        //initialize the spring
+        spring = gameObject.AddComponent<SpringJoint>();
+        //spring.spring = springForce;
+        //spring.damper = damper;
+        spring.maxDistance = maxDistance;
+        spring.minDistance = minDistance;
+        spring.connectedBody = tornadoRef.gameObject.GetComponent<Rigidbody>();
+
+        //StartCoroutine(refreshImpulse());
+    }
+
+    IEnumerator refreshImpulse()
+    {
+        transform.RotateAround(tornadoReference.transform.position, tornadoReference.GetRotationAxis(), tornadoReference.GetTornadoStrength());
+        yield return new WaitForSeconds(0.5f);
+    }
+}
---
+++ Assets/Scripts/TornadoScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TornadoScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private float springForce, damper, maxDistance, minDistance;
+    [SerializeField]
+    private float tornadoStrength = 20;
+    [SerializeField]
+    private Vector3 rotationAxis = new Vector3(0, 1, 0);
+
+	// Use this for initialization
+	void Start ()
+	{
+        rotationAxis.Normalize();
+	}
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.GetComponent<Caught>() && other.attachedRigidbody)
+        {
+            Caught caught = other.gameObject.AddComponent<Caught>();
+            caught.Init(this, springForce, damper, maxDistance, minDistance);
+       }
+    }
+
+    public Vector3 GetRotationAxis()
+    {
+        return rotationAxis;
+    }
+
+    public float GetTornadoStrength()
+    {
+        return tornadoStrength;
+    }
+}
